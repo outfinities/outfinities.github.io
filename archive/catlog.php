8b80344580b4de983cf064130a5844cdc799b859
Outfinities <72750770+outfinities@users.noreply.github.com>
2020-10-16 20:27:48 +0800

    Update catlog.php

--- archive/catlog.php
+++ archive/catlog.php
@@ -1,24 +1,16 @@
-<?php 
-
- 
-
-function tree($directory) 
-{ 
-$mydir=dir($directory); 
-echo "<ul>"; 
-while($file=$mydir->read()){ 
-if((is_dir("$directory/$file")) AND ($file!=".") AND ($file!="..")){ 
-echo "<li><font color='#ff00cc'><b>$file</b></font></li>"; 
-tree("$directory/$file"); 
-}else{ 
-echo "<li><a href=$directory/$file>$file</a></li>"; 
-} 
-} 
-echo "</ul>"; 
-$mydir->close(); 
-} 
-//start the program 
-echo "<h2>目录为粉红色</h2>"; 
- 
-tree("."); 
-?> 
+<?php
+       echo "<li>download files list<li>";
+       if ($handle = opendir("./"))
+       {
+          while (1)
+          {
+                $ufile = readdir($handle);
+                if ($ufile == "") break;
+                if (($ufile!=".")&($ufile!= ".."))
+                {
+                      $tfile=iconv('GB2312', 'UTF-8', $ufile);
+                      echo "<li><a href='".$config_basedir_file.$tfile."'>URL+".$tfile."</a></li>";
+                }
+          }
+    }
+?>
